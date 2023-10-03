1ea4731bd036b259a75a99f218c7c425831841c0
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-10-03 00:08:15 -0300

    refatorando código pra manipulação de listas

--- other/lista.c
+++ other/lista.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct cel {
   int value;
   struct cel *next;
 } cel;
 
-cel *create_cell(int valor) {
-  // aloca espaço na memporia e aponta cel pra esse endereço
-  cel *celula = (cel *)malloc(sizeof(cel));
-
-  celula->value = valor;
-  celula->next = NULL;
-  return celula;
-}
-
 cel* add_cell(cel *p, int valor, int posicao) {
   cel *new_p = (cel *)malloc(sizeof(cel));
+  
+  new_p->value = valor;
+  
   // insere antes
   if (posicao == 1){
-    new_p->value = valor;
     new_p->next = p;
     return new_p;
   }
   // insere depois
   else {
     new_p->next = p->next;
-    new_p->value = valor;
     p->next = new_p;
     return p;
   }
@@ -40,12 +33,17 @@ void remove_cell(cel *p) {
 int main() {
     printf("salve\nesse é meu algoritmo pra manipulação de celulas");
 
-    printf("Criando célula");
-    cel celula = create_cell(69);
-    printf("adicionando célula");
-    cel * myCell;
-    myCell = &celula;
-    myCell = (myCell,96,1);
-    
-    printf("");
+  cel *celula = (cel *)malloc(sizeof(cel));
+
+  celula->value = 69;
+  celula->next = NULL;
+
+  // criando fila
+  for (int i = 0; i < 4; i++){
+  cel *p = celula;
+  p = add_cell(p,i,2);
+  }
+  
+  printf("lista criada com sucesso");
+  return 0;
 }
