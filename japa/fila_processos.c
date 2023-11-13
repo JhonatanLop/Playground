bc72070b31feb7e0c63d1c7f3ede95338daafe65
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-11-13 17:03:34 -0300

    refatorando manipulação de fila

--- japa/fila_processos.c
+++ japa/fila_processos.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAX_NAME_LENGTH 100
 
 typedef struct cel {
-  char algoritmo;
+  int algoritmo[];
   struct cel *next;
 } cel;
 
 // insere pessoa na fila
-void inserir_comando(char comando, cel *init, cel *fim){
+void inserir_comando(int comando, cel *init, cel *fim){
   cel *new = (cel *)malloc(sizeof(cel));
   new->algoritmo = comando;
   new->next = NULL;
@@ -20,53 +19,50 @@ void inserir_comando(char comando, cel *init, cel *fim){
   }
   else
   {
-    new->next = init;
-    init = new;
+    new->next = init->next;
+    fim->next = new;
   }
 }
 
-char extrair_comando(cel *fim){
-  if (fim->next == NULL){
+int extrair_comando(cel *init){
+  if (init->next == NULL){
     printf("Sem comandos na fila\n");
     exit(1);
   } else{
-    cel *p = fim->next;
-    fim->next = fim->next->next;
-    char command = p->algoritmo;
+    cel *p = init->next;
+    init->next = init->next->next;
+    int command = p->algoritmo;
     free(p);
     return command;
   }
 }
 
-// imprime a fila
-void print(cel *fim){
-  cel *aux = fim->next;
-  while (aux->next != NULL) {
-    printf("%c\n", aux->algoritmo);
-    aux = aux->next;
+//printa a pilha
+void print(cel*fim){
+
+  cel *act = fim->next;
+  while (act->next != NULL)
+  {
+    printf("\n%c", act->algoritmo);
+    act = act->next;
   }
-  printf("%c\n", aux->algoritmo);
+  printf("\n%c", act->algoritmo);
 }
 
 // função que testa o algoritmo
 int main(){
-  char c1 = 'a';
-  char c2 = 'b';
-  char c3 = 'c';
-  char c4 = 'd';
-  char c5 = 'e';
+  int c1 = 'a';
+  printf("Comando: %c\n", fim->algoritmo);
+  printf("Comando: %c\n", fim->next->algoritmo);
+  int c2 = 'b';
+  int c3 = 'c';
+  int c4 = 'd';
+  int c5 = 'e';
 
   cel *fim;
   cel *init;
-  // fim->next = NULL;
-  // init->next = NULL;
-
-  cel *proc1 = (cel *)malloc(sizeof(cel));
-  fim = proc1;
-  init = proc1;
-  proc1->algoritmo = '0';
-  proc1->next = NULL;
-
+  fim = NULL;
+  init = NULL;
 
   inserir_comando(c1, init, fim);
   inserir_comando(c2, init, fim);
@@ -76,16 +72,16 @@ int main(){
   print(fim);
 
 
-  // char comando = extrair_comando(fim);
+  // int comando = extrair_comando(fim);
   // printf("\nComando extraido: %c\n", comando);
 
-  // char comandoa = extrair_comando(fim);
+  // int comandoa = extrair_comando(fim);
   // printf("\nComando extraido: %c\n", comandoa);
 
   print(fim);
 
   printf("\nInserindo comando 'f'\n");
-  char c6 = 'f';
+  int c6 = 'f';
   inserir_comando(c6, init, fim);
 
   print(fim);
