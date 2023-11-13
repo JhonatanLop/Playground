1b07d0768bd7dbd45268b5f5c0a14e4a38095601
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-11-13 20:50:33 -0300

    refatorando manipulação de fila

--- japa/fila_processos.c
+++ japa/fila_processos.c
@@ -3,29 +3,28 @@
 #include <stdlib.h>
 
 typedef struct cel {
-  int algoritmo[];
+  int algoritmo;
   struct cel *next;
 } cel;
 
+
 // insere pessoa na fila
-void inserir_comando(int comando, cel *init, cel *fim){
-  cel *new = (cel *)malloc(sizeof(cel));
-  new->algoritmo = comando;
+void inserir_comando(int command, cel**init, cel**fim){
+  cel *new = (cel*)malloc(sizeof(cel));
+  new->algoritmo = command;
   new->next = NULL;
-  if(fim->next == NULL)
-  {
-    init->next = new;
-    fim->next = new;
-  }
-  else
-  {
-    new->next = init->next;
-    fim->next = new;
+
+  if (*init == NULL) {
+    *init = new;
+    *fim = new;
+  } else {
+    (*fim) = new;
+    *fim = new;
   }
 }
 
 int extrair_comando(cel *init){
-  if (init->next == NULL){
+  if (init){
     printf("Sem comandos na fila\n");
     exit(1);
   } else{
@@ -51,25 +50,23 @@ void print(cel*fim){
 
 // função que testa o algoritmo
 int main(){
+
   int c1 = 'a';
-  printf("Comando: %c\n", fim->algoritmo);
-  printf("Comando: %c\n", fim->next->algoritmo);
   int c2 = 'b';
   int c3 = 'c';
   int c4 = 'd';
   int c5 = 'e';
 
-  cel *fim;
-  cel *init;
-  fim = NULL;
-  init = NULL;
+  cel **init = NULL;
+  cel **fim = NULL;
 
   inserir_comando(c1, init, fim);
+  printf("Comando: %c\n", fim);
   inserir_comando(c2, init, fim);
   inserir_comando(c3, init, fim);
   inserir_comando(c4, init, fim);
   inserir_comando(c5, init, fim);
-  print(fim);
+  // print(fim);
 
 
   // int comando = extrair_comando(fim);
@@ -78,13 +75,13 @@ int main(){
   // int comandoa = extrair_comando(fim);
   // printf("\nComando extraido: %c\n", comandoa);
 
-  print(fim);
+  // print(fim);
 
-  printf("\nInserindo comando 'f'\n");
-  int c6 = 'f';
-  inserir_comando(c6, init, fim);
+  // printf("\nInserindo comando 'f'\n");
+  // int c6 = 'f';
+  // inserir_comando(c6, init, fim);
 
-  print(fim);
+  // print(fim);
 
   return 0;
 }
