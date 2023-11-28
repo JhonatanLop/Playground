004ecf284e9712df4fcb8c44b82fde42d3bcfd70
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-11-27 22:17:52 -0300

    terminando manipulação de fila

--- japa/fila_processos.c
+++ japa/fila_processos.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Estrutura principal
-typedef struct cel {
+// Estrutura principal
+typedef struct cel
+{
   int conteudo;
   struct cel *next;
 } cel;
 
-//Insere conteudo na fila
-void Inserir(int conteudo, cel **fim, cel **init) {
-  cel *new = (cel*)malloc(sizeof(cel));
+// Insere conteudo na fila
+void fazer_pedido(int conteudo, cel **fim, cel **init)
+{
+  cel *new = (cel *)malloc(sizeof(cel));
   new->conteudo = conteudo;
   new->next = NULL;
 
-  //Se estiver vazia
-  if (*init == NULL) {
+  // Se estiver vazia
+  if (*init == NULL)
+  {
     *init = new;
     *fim = new;
-  } else { //Se não estiver vazia
-    *fim->next = new;
+  }
+  else
+  { // Se não estiver vazia
+    (*fim)->next = new;
     *fim = new;
   }
 }
 
-//Remover e retornar o primeiro elemento da fila
-int Extrair(cel **init) {
-  if(init == NULL) {
+// Remover e retornar o primeiro elemento da fila
+int entregar_pedido(cel **init)
+{
+  if (init == NULL)
+  {
     printf("Sem conteudos na fila\n");
     exit(1);
-  } else {
+  }
+  else
+  {
     int command = (*init)->conteudo;
     cel *p = *init;
     *init = (*init)->next;
@@ -37,46 +46,52 @@ int Extrair(cel **init) {
   }
 }
 
-//Imprimir a fila
-void PrintLista(cel *init) {
+// Imprimir a fila
+void PrintLista(cel *init)
+{
   cel *p = init;
-  while(p != NULL) {
-    printf("%d->", p->conteudo);
+  while (p != NULL)
+  {
+    printf("<- | pedido %d | ", p->conteudo);
     p = p->next;
-  } printf("\n");
+  }
+  printf("\n");
 }
 
-//Liberar a memória
-void LiberarLista(cel **init) {
-  while(init != NULL) {
+// Liberar a memória
+void LiberarLista(cel **init)
+{
+  while (*init != NULL)
+  {
     cel *p = *init;
     *init = (*init)->next;
     free(p);
   }
 }
 
-//Função principal
-int main(void) {
+// Função principal
+int main(void)
+{
   cel *init = NULL;
   cel *fim = NULL;
 
-  Inserir(3);
-  Inserir(2);
-  Inserir(7);
-  Inserir(2);
-  Inserir(3);
-  Inserir(5);
-  Inserir(9);
-  Inserir(8);
-  Inserir(2);
+  fazer_pedido(1, &fim, &init);
+  fazer_pedido(2, &fim, &init);
+  fazer_pedido(3, &fim, &init);
+  fazer_pedido(4, &fim, &init);
+  fazer_pedido(5, &fim, &init);
+  fazer_pedido(6, &fim, &init);
+  fazer_pedido(7, &fim, &init);
+  fazer_pedido(8, &fim, &init);
+  fazer_pedido(9, &fim, &init);
 
-  PrintLista();
+  PrintLista(init);
 
-  Extrair();
-  Extrair();
+  entregar_pedido(&init);
+  entregar_pedido(&init);
 
-  PrintLista();
-  
-  LiberarLista();
+  PrintLista(init);
+
+  LiberarLista(&init);
   return 0;
 }
