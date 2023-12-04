5101615e66fea1fd81418c97003664fab0b11208
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-12-04 16:51:59 -0300

    adicionando calculo do cubo e teste

---
+++ Java/areachecker/src/main/java/math/area/Classes/Cube.java
@@ -0,0 +1,23 @@
+package math.area.Classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Cube {
+    double side;
+    double area;
+
+    public void calculate_area(Cube cube) {
+        // formula para cubo
+        // lado³
+        double area = cube.getSide() * cube.getSide() * cube.getSide();
+        cube.setArea(area);
+    }
+}
---
+++ Java/areachecker/src/test/java/math/area/CubeTest.java
@@ -0,0 +1,16 @@
+package math.area;
+
+import org.junit.Test;
+
+import math.area.Classes.Cube;
+
+public class CubeTest {
+    
+    @Test
+    public void testCalculateArea() {
+        Cube cube = new Cube();
+        cube.setSide(5);
+        cube.calculate_area(cube);
+        assert(cube.getArea() == 125);
+    }
+}
