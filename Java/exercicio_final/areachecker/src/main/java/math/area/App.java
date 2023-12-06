1a3a7ef7e294dcca1cd35dd90d8268ca4d32665c
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-12-06 15:37:13 -0300

    alterando main, criando a classe shapefactory e o enum shapetype

--- Java/exercicio_final/areachecker/src/main/java/math/area/App.java
+++ Java/exercicio_final/areachecker/src/main/java/math/area/App.java
@@ -1,35 +1,35 @@
 package math.area;
 
 import math.area.interfaces.Calculable;
-import math.area.interfaces.CalculableFactory;
-import math.area.shapes.CalculableCreator;
+import math.area.shapes.ShapeFactory;
+import math.area.shapes.ShapeType;
 
 public class App
 {
     public static void main( String[] args )
     {
-        CalculableFactory factory = new CalculableCreator();
         
         // dados de exemplo
-        Calculable quadrado = factory.createSquare(5);
-        Calculable retângulo = factory.createRectangle(8,3);
-        Calculable círculo = factory.createCircle(5);
+        Calculable quadrado = ShapeFactory.getShape(ShapeType.SQUARE, new Double[]{5.0});
+        Calculable retangulo = ShapeFactory.getShape(ShapeType.RECTANGLE, new Double[]{8.0, 3.0});
+        Calculable círculo = ShapeFactory.getShape(ShapeType.CIRCLE, new Double[]{5.0});
         Double[] sides = new Double[]{3.0, 4.0, 5.0};
-        Calculable triângulo = factory.createTriangle(sides);
-        Calculable paralelograma = factory.createParallelogram(5, 3);
-        Calculable trapézio = factory.createTrapeze(5, 3, 4);
-        Calculable hexágono = factory.createHexagon(5);
-        Calculable losango = factory.createRhombus(5, 3);
-        Calculable cubo = factory.createCube(5);
+        Calculable triângulo = ShapeFactory.getShape(ShapeType.TRIANGLE, sides);
+        Calculable paralelograma = ShapeFactory.getShape(ShapeType.PARALLELOGRAM, new Double[]{5.0, 3.0});
+        Calculable trapezio = ShapeFactory.getShape(ShapeType.TRAPEZE, new Double[]{5.0, 3.0, 4.0});
+        Calculable hexagono = ShapeFactory.getShape(ShapeType.HEXAGON, new Double[]{5.0});
+        Calculable losango = ShapeFactory.getShape(ShapeType.RHOMBUS, new Double[]{5.0, 3.0});
+        Calculable cubo = ShapeFactory.getShape(ShapeType.CUBE, new Double[]{5.0});
+
 
         // saída de dados
         System.out.println("Área do quadrado: " + quadrado.calcularArea());
-        System.out.println("Área do retângulo: " + retângulo.calcularArea());
+        System.out.println("Área do retângulo: " + retangulo.calcularArea());
         System.out.println("Área do círculo: " + círculo.calcularArea());
         System.out.println("Área do triângulo: " + triângulo.calcularArea());
         System.out.println("Área do paralelograma: " + paralelograma.calcularArea());
-        System.out.println("Área do trapézio: " + trapézio.calcularArea());
-        System.out.println("Área do hexágono: " + hexágono.calcularArea());
+        System.out.println("Área do trapézio: " + trapezio.calcularArea());
+        System.out.println("Área do hexágono: " + hexagono.calcularArea());
         System.out.println("Área do losango: " + losango.calcularArea());
         System.out.println("Área do cubo: " + cubo.calcularArea());
 
